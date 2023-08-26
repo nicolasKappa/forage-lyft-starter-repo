4a46a7f07e57bf43549eb6d8de641966a8c169f2
nicolasKappa <nicola.kappa8@gmail.com>
2023-08-27 03:49:38 +0400

    Lyft Task

--- car.py
+++ car.py
@@ -1,9 +1,13 @@
+"""imports how service status is assesed"""
+
 from serviceable import Serviceable
 
 class Car(Serviceable):
+    """tells if it is servicable according to engine and battery info"""
     def __init__(self, engine, battery):
         self.engine = engine
         self.battery = battery
 
     def needs_service(self):
         return self.engine.needs_service() or self.battery.needs_service()
+    
--- engine/model/calliope.py
+++ engine/model/calliope.py
@@ -6,6 +6,10 @@ from engine.capulet_engine import CapuletEngine
 
 class Calliope(CapuletEngine):
     """united data for Capulet engine on Calliope car model"""
+    def __init__(self, last_service_date, current_mileage, last_service_mileage):
+        super().__init__(last_service_date, current_mileage, last_service_mileage)
+        self.last_service_date = last_service_date
+        
     def needs_service(self: any) -> bool:
         """tells for Capulet engine on Calliope car model when service is needed"""
         service_threshold_date = self.last_service_date.replace(
