b872ed22afcff81d55d287cb7444d2a437810edb
nicolasKappa <nicola.kappa8@gmail.com>
2023-08-27 03:52:31 +0400

    Lyft Task

--- engine/model/calliope.py
+++ engine/model/calliope.py
@@ -9,7 +9,6 @@ class Calliope(CapuletEngine):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
         super().__init__(last_service_date, current_mileage, last_service_mileage)
         self.last_service_date = last_service_date
-        
     def needs_service(self: any) -> bool:
         """tells for Capulet engine on Calliope car model when service is needed"""
         service_threshold_date = self.last_service_date.replace(
