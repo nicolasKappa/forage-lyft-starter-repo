99f9485da42086980ece7bd243fd53bfdcca32aa
nicolasKappa <nicola.kappa8@gmail.com>
2023-08-27 02:18:06 +0400

    Lyft Task

--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
-def add_years_to_date(original_date, years_to_add):
+def add_years_to_date(original_date: int, years_to_add: int) -> int:
     result = original_date.replace(year=original_date.year + years_to_add)
     return result
